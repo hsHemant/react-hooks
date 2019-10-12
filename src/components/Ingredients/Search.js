74eb4057e00b9d18036198e11d362c54a13f4b8e
shHemant <hemant80555@gmail.com>
2019-10-12 12:19:10 +1100

    search component udpate

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import './Search.css';
+import Card from "../UI/Card";
+import "./Search.css";
 
 const Search = React.memo(props => {
+  const [searchValue, setSearchValue] = useState("");
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={searchValue}
+            onChange={event => {
+              setSearchValue(event.target.value);
+            }}
+          />
         </div>
       </Card>
     </section>
